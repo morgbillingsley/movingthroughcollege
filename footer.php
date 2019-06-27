f2cfb4a1ad877df331539187a99c90fd9b96aa04
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-27 15:17:50 -0700

    header/footer i commit

---
+++ footer.php
@@ -0,0 +1,71 @@
+      <!-- footer -->
+      <footer id="footer">
+         <div id="footer-widgets" class="container style-1">
+            <div class="row">
+               <div class="col-md-4">
+                  <div class="widget widget_text">
+                     <h2 class="widget-title"><span>ABOUT US</span></h2>
+                     <div class="textwidget">
+                        <p>Aenean suscipit eget mi act fermentum phasellus vulputate turpis tincidunt. Aenean suscipit eget. Aenean suscipit eget mi act fermentum phasellus vulputate turpis tincidunt. Aenean suscipit ege  Aenean suscipit eget mi act fermentum phasellus.</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="widget widget_links">
+                     <h2 class="widget-title"><span>COMPANY LINKS</span></h2>
+                     <ul class="wprt-links clearfix col2">
+                        <li><a data-scroll="" href="#home">Home</a></li>
+                        <li><a data-scroll="" href="#about">About Us</a></li>
+                        <li><a data-scroll="" href="#services">Services</a></li>
+                        <li><a data-scroll="" href="#contact">Contact Us</a></li>
+                        <li><a data-scroll="" href="#testimonials">Reviews</a>
+					 </ul>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="widget widget_information">
+                     <h2 class="widget-title"><span>CONTACT INFO</span></h2>
+                     <ul>
+                        <li class="phone clearfix">
+                           <span class="hl">Phone:</span> 
+                           <span class="text">+1(602) 799-8605</span>
+                        </li>
+                        <li class="email clearfix">
+                           <span class="hl">E-mail:</span>
+                           <span class="text">rubendrotz@movingthroughcollege.com</span>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="widget widget_socials">
+                     <div class="socials">
+                        <a target="_blank" href="#"><i class="fa fa-yelp"></i></a>
+                        <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div id="bottom" class="clearfix style-1">
+            <div class="container">
+               <div id="bottom-bar-inner" class="wprt-container">
+                  <div class="bottom-bar-inner-wrap">
+                     <div class="bottom-bar-content">
+                        <div id="copyright">All Rights Reserved. Moving Through College © <?php echo date('Y'); ?></div>
+                        <!-- /#copyright -->
+                     </div>
+                     <!-- /.bottom-bar-menu -->
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <a href="#home" data-scroll class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+      <!-- ALL JS FILES -->
+      <script src="<?php bloginfo('template_url') ?>/js/all.js"></script>
+      <!-- ALL PLUGINS -->
+      <script src="<?php bloginfo('template_url') ?>/js/custom.js"></script>
+      <script src="<?php bloginfo('template_url') ?>/js/portfolio.js"></script>
+      <script src="<?php bloginfo('template_url') ?>/js/hoverdir.js"></script>   
+   </body>
+</html>
---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <meta charset="<?php bloginfo('charset') ?>">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+   <!-- Site Metas -->
+   <title><?php bloginfo('name') ?></title>
+   <meta name="description" content="<?php bloginfo('description') ?>">
+   <!-- Site Icons -->
+   <link rel="shortcut icon" href="<?php bloginfo('template_url') ?>/images/fevicon.ico.png" type="image/x-icon" />
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/bootstrap.min.css">
+   <!-- Site CSS -->
+   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
+   <!-- Colors CSS -->
+   <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/colors.css">
+   <!-- ALL VERSION CSS -->
+   <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/versions.css">
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/responsive.css">
+   <!-- building CSS -->
+   <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/building.css">
+   </head>
+   <body class="building_version" data-spy="scroll" data-target=".header">
+      <!-- LOADER -->
+      <div id="preloader">
+         <img class="preloader" src="<?php bloginfo('template_url') ?>/images/loaders/loader-building2.gif" alt="">
+      </div>
+      <!-- end loader -->
+      <!-- END LOADER -->
+      <header class="header header_style_01">
+         <nav class="megamenu navbar navbar-default" data-spy="affix">
+            <div class="top-header">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-hidden">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-hidden">
+                           <div class="gem-contacts-item gem-contacts-phone">
+                              <a class="phone-icon" href="#" target="_blank" title="phone"><i class="fa fa-phone" aria-hidden="true"></i>+1(602) 799-8605</a>
+                           </div>
+                        </div>
+                        <div class="top-area-block top-area-socials socials-colored-hover">
+                           <div class="socials inline-inside"> 
+                              <a class="socials-item" href="#" target="_blank" title="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                              <a class="socials-item" href="#" target="_blank" title="linkedin"><i class="fa fa-yelp" aria-hidden="true"></i></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="container">
+               <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="index.html"><img src="images/logo-icon.png" alt="image"><span>Moving Through College</span></a>
+               </div>
+               <div id="navbar" class="navbar-collapse collapse">
+                  <ul class="nav navbar-nav navbar-right">
+                     <li class="active"><a data-scroll href="#home">Home</a></li>
+                     <li><a data-scroll href="#about">About Us</a></li>
+                     <li><a data-scroll href="#services">Services</a></li>
+                     <li><a data-scroll href="#contact">Contact</a></li>
+                     <li><a data-scroll href="#testimonials">Reviews</a></li>
+                  </ul>
+               </div>
+            </div>
+         </nav>
+      </header>
