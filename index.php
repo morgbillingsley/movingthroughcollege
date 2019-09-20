95f1475705ca29cb098f4e28314154e6e97a61d5
morgbillingsley <billingsley98.mb@gmail.com>
2019-09-19 22:14:14 -0700

    services ii commit

--- index.php
+++ index.php
@@ -111,7 +111,7 @@
                   </div>
                   <div class="serv-blog-cont">
                      <h3>Moving Labor</h3>
-                     <p>Local Moves Only (Phoenix Metropolitan Area)<br>
+                     <p><small>Local Moves Only (Phoenix Metropolitan Area)</small><br>
                         Is good for long distance moves where you want to do the driving yourself but don’t want to do the work of loading all the heavy furniture by yourself</p>
                   </div>
                </div>
@@ -121,7 +121,7 @@
                   </div>
                   <div class="serv-blog-cont">
                      <h3>Labor &amp Truck</h3>
-                     <p>Local Moves Only (Phoenix Metropolitan Area)<br>
+                     <p><small>Local Moves Only (Phoenix Metropolitan Area)</small><br>
                         Is an affordable way to have all your belongs moved without having to deal with any of the heavy lifting or driving a giant truck.</p>
                   </div>
                </div>
@@ -131,7 +131,7 @@
                   </div>
                   <div class="serv-blog-cont">
                      <h3>Long Distance</h3>
-                     <p>100+ Miles (Nation Wide, Not just Phoenix)<br>
+                     <p><small>100+ Miles (Nation Wide, Not just Phoenix)</small><br>
                         Is for moves +100 miles. We will have moving truck, load, drive, and unload your belongings in a sufficient time.</p>
                   </div>
                </div>
