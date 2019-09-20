75d8bda335a07d78dffd322f9df1add85cd6a8d3
morgbillingsley <billingsley98.mb@gmail.com>
2019-09-20 00:31:46 -0700

    email iv commit

--- functions.php
+++ functions.php
@@ -112,8 +112,9 @@
 
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $apikey", 'Content-Type: application/json'));
-        curl_setopt ($curl, CURLOPT_POST, true);
-        curl_setopt ($curl, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
         $response = curl_exec($curl);
         curl_close($curl);
