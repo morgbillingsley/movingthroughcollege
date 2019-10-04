422c5bf2d6d8d4ec021f755f220fe9723c73d452
morgbillingsley <billingsley98.mb@gmail.com>
2019-10-03 17:48:11 -0700

    phpmailer

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '/var/www/html/vendor/autoload.php';
     // Theme Support
     add_theme_support('post-thumbnails');
 
@@ -7,7 +12,8 @@
     add_action('admin_post_contact', 'contact_proc');
 
     // Functions
-    function contact_proc() {
+    function contact_proc()
+    {
         // Post data
         $fname = strval($_POST['fname']);
         $lname = strval($_POST['lname']);
@@ -85,13 +91,14 @@
             </tr>
         </table>";
         $plain = "Name: $fname $lname\nEmail: $email\nPhone: $phone\nPacking: $packing\nService: $service\nTruck: $truck\nMessage: $message";
-        sendgrid($to, $subject, $plain);
+        sendEmail($to, $subject, $html, $plain);
 
         // Redirect to homepage
         wp_redirect('http://movingthroughcollege.com/');
     }
     
-    function pushover($message) {
+    function pushover($message)
+    {
         $params = array(
             "token" => "adcuf77pykadwm6zyj14b9vfyhhy84",
             "user" => "u4r33u7gk4txdjfcv1m2qxjnwrmag4",
@@ -106,30 +113,33 @@
         curl_close($po);
     }
 
-    function sendgrid($to, $subject, $body) {
-        $apikey = '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';
-        $url = 'https://api.sendgrid.com/v3/mail/send';
-        // $params = "{\'personalizations\': [{\'to\': [{\'email\': \'$to\'}]}],\'from\': {\'email\': \'rubendrotz@movingthroughcollege.com\'},\'subject\': \'$subject\',\'content\': [{\'type\': \'text/html\', \'value\': \'$body\'}]}";
-        $params = array(
-            'personalizations' => array(
-                'to' => array('email' => $to)
-            ),
-            'from' => array('email' => 'rubendrotz@movingthroughcollege.com'),
-            'subject' => $subject,
-            'content' => array(
-                'type' => 'text/plain',
-                'value' => $body
-            )
-        );
-        $json = json_encode($params);
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $apikey", 'Content-Type: application/json'));
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    function sendEmail($to, $subject, $html, $plain)
+    {
+        $mail = new PHPMailer(true);
 
-        $response = curl_exec($curl);
-        curl_close($curl);
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.yandex.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'rubendrotz@movingthroughcollege.com';                     // SMTP username
+            $mail->Password   = 'drotz498';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTSSL;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = 465;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('rubendrotz@movingthroughcollege.com', 'Ruben Drotzmann');
+            $mail->addAddress($to);
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body    = $html;
+            $mail->AltBody = $plain;
+
+            $mail->send();
+        } catch (Exception $e) {
+            die($e);
+        }
     }
-?>
